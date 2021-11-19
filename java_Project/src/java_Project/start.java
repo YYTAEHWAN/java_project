fc031d42bdf2400389879195123e7065e8ea55ff
YYTAEHWAN <ygen1002@gmail.com>
2021-11-19 14:56:31 +0900

    this is test!

---
+++ java_Project/src/java_Project/start.java
@@ -0,0 +1,7 @@
+package java_Project;
+
+public class start {
+	public static void main(String[] args) {
+		System.out.println("before starting to java project. we have to look the blueprint");
+	}
+}
